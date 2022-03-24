52dddeb576c5f9524976acd0c1d9fd7e61b56a1f
AppleLi <vidorxin@gmail.com>
2022-03-24 01:15:18 -0400

    poison

--- Assets/Resources/Apple/Script/SpeedDownZone.cs
+++ Assets/Resources/Apple/Script/SpeedDownZone.cs
@@ -9,7 +9,7 @@ public class SpeedDownZone : Tile
     {
         if (collider.gameObject.tag == "player")
         {
-            Debug.Log("SpeedUp");
+            Debug.Log("SpeedDown");
             GetComponent<Player>().moveSpeed -= 4;
         }
     }
