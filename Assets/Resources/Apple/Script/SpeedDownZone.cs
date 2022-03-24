ea0eb24ef56393549ee564347e1ad2dd35e02113
Tim Chen <40655433+cty288@users.noreply.github.com>
2022-03-24 01:08:51 -0400

    merge1

---
+++ Assets/Resources/Apple/Script/SpeedDownZone.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedDownZone : Tile
+{
+    private void Start() {
+        GetComponent<SpriteRenderer>().sortingOrder = -20;
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("SpeedUp");
+            collider.GetComponent<Player>().moveSpeed -= 4;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("SpeedUp");
+            collider.GetComponent<Player>().moveSpeed += 4;
+        }
+    }
+}
